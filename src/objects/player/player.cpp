c644f5c594e4f7f2ccda3ab4c3fda780d0e1fc1f
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-07-26 03:19:26 -0300

    creacion de clase object referente a todo objeto fisico en escena

--- src/objects/player/player.cpp
+++ src/objects/player/player.cpp
@@ -35,27 +35,6 @@ void Player::Move(float timeScale)
 		body.y -= (speed * GetFrameTime()) * timeScale;
 	}
 }
-void Player::Draw()
-{
-	DrawRectangle(static_cast<int>(body.x), static_cast<int>(body.y), 
-				  static_cast<int>(body.width), static_cast<int>(body.height), myColor);
-}
-void Player::SetX(float newX)
-{
-	body.x = newX;
-}
-float Player::GetX()
-{
-	return body.x;
-}
-void Player::SetY(float newY)
-{
-	body.y = newY;
-}
-float Player::GetY()
-{
-	return body.y;
-}
 void Player::SetLife(int newLife)
 {
 	life = newLife;
--- src/objects/player/player.h
+++ src/objects/player/player.h
@@ -1,25 +1,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "raylib.h"
+#include "objects/object.h"
 namespace railgun
 {
-class Player
+class Player : public Object
 {
 public:
 	Player();
 	~Player();
 	void Input(float timeScale);
 	void Move(float timeScale);
-	void Draw();
-	void SetX(float newX);
-	float GetX();
-	void SetY(float newY);
-	float GetY();
 	void SetLife(int newLife);
 	int GetLife();
 private:
-	Rectangle body;
-	Color myColor = RED;
 	int life;
 	float width = 50.0f;
 	float height = 50.0f;
